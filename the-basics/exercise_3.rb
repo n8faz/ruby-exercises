8416fcf9175f73bba40db7773a8385c915110f20
n8faz <119473201+n8faz@users.noreply.github.com>
2023-04-18 16:01:30 -0700

    Complete exercise 3

---
+++ the-basics/exercise_3.rb
@@ -0,0 +1,9 @@
+movie_year = {:Jaws => 1975,
+:Spiderman => 2002,
+:Avengers => 2012,
+:Rushmore => 1998}
+
+puts movie_year[:Jaws]
+puts movie_year[:Spiderman]
+puts movie_year[:Avengers]
+puts movie_year[:Rushmore]
