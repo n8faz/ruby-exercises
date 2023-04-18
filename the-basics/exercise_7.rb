1133851c2f93624e97047b28a37394757f8444fc
n8faz <119473201+n8faz@users.noreply.github.com>
2023-04-18 16:26:31 -0700

    Complete exercise 7

---
+++ the-basics/exercise_7.rb
@@ -0,0 +1,5 @@
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+#  from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
+
+# There is an error in the syntax where a closing parentheses 
+# was placed instead of a curly bracket or no closing bracket at all
