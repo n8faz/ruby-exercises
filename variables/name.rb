6b376ee44358d0bc8830f36207dd9cca995350e1
n8faz <119473201+n8faz@users.noreply.github.com>
2023-04-18 18:11:22 -0700

    Complete exercise 4

--- variables/name.rb
+++ variables/name.rb
@@ -1,8 +1,11 @@
 
 
-puts "What is your name? "
-name = gets.chomp
+puts "What is your first name? "
+first_name = gets.chomp
 
-puts "Hello, #{name}!"
+puts "What is your last name?"
+last_name = gets.chomp
 
-10.times { puts name }
+puts "Hello, #{first_name} #{last_name}!"
+
+10.times { puts first_name + " " + last_name }
