1168548625a380820c7e08cdaaa72e3734cf51f9
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-16 18:21:33 -0700

    Update greeting.rb

--- more-stuff/greeting.rb
+++ more-stuff/greeting.rb
@@ -1,6 +1,14 @@
-def greet(person)
-  puts "Hello, " + person
+def space_out_letters(person)
+  return person.split("").join(" ")
 end
 
-greet("John")
-greet(1)
+def greet(person)
+  return "H e l l o, " + space_out_letters(person)
+end
+
+def decorate_greeting(person)
+  puts "" + greet(person) + ""
+end
+
+decorate_greeting("John")
+decorate_greeting(1)
