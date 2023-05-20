5320289fc2eb3963b44f0b4ef23e90578438a1fc
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-19 21:31:22 -0700

    Complete exercise 10

---
+++ end-exercises/exercise_10.rb
@@ -0,0 +1,15 @@
+#Can hash values be arrays?
+# Yes hash values can be arrays
+
+h = {a:["this", "is", "an", "array"], b:["as", "a", "value"]}
+
+#Can you have an array of hashes?
+# Yes an array can be composed of hashes
+
+a = [{a:1, b:2}, {c:3, d:4}]
+
+
+
+
+p h
+p a
